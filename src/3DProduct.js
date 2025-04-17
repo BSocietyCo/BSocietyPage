97e0a31139aad2f2d94dee06bf9a32fae8f394fd
BSociety <deploybsociety@gmail.com>
2025-04-17 08:45:03 -0500

    Fix product

--- src/3DProduct.js
+++ src/3DProduct.js
@@ -2,8 +2,8 @@ import Spline from "@splinetool/react-spline";
 
 export function Modelo3DCadena() {
   return (
-    <section className="w-full h-screen bg-black flex items-center justify-center px-4">
-      <div>
+    <section className="w-full h-screen bg-black flex items-center justify-center overflow-hidden">
+      <div className="w-full h-full max-w-none">
         <Spline scene="https://prod.spline.design/nF3V49fMMwfMZvVI/scene.splinecode" />
       </div>
     </section>
